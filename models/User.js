7ef79f1b3046cee33a18b2ba747439191cff6877
austin-bagwell <austinbagwell.dev@gmail.com>
2023-02-06 20:48:19 -0500

    building basic User model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,28 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import Consignee from "./Consignee";
+
+const { Schema } = mongoose;
+
+const userSchema = Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  consignees: [{ id: String, name: String, transitTime: String }],
+
+  // TODO CRUD ops for consignees as static methods on this class/model
+  //   User.addConsignee({
+  //     this.consignees.push({details of a new consignee})
+  // })
+  //
+});
+
+const User = mongoose.model("User", userSchema);
