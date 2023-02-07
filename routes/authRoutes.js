0160099e82646b74d4b2232cf6b6c7a5b46beab1
austin-bagwell <austinbagwell.dev@gmail.com>
2023-02-06 21:13:25 -0500

    add authRoutes

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = new Router();
+
+// callback will be definedin authControllers.js
+router.get("/signup", (req, res) => {});
+router.post("/signup", (req, res) => {});
+router.get("/login", (req, res) => {});
+router.post("/login", (req, res) => {});
+router.get("/consignees", (req, res) => {});
+router.post("/consignees", (req, res) => {});
+
+export { router };
